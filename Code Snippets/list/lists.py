edb26e62847b042f5197838a2364126a07884de0
pilakaravi.rs143@gmail.com <pilakaravi.rs143@gmail.com>
2024-11-28 18:57:11 +0530

    remove numbering to questions

--- Code Snippets/list/lists.py
+++ Code Snippets/list/lists.py
@@ -1,22 +1,22 @@
 # Lists
 
-# 8. Creating a List
+#Creating a List
 fruits = ["apple", "banana", "cherry"]
 print(fruits)  # Output: ['apple', 'banana', 'cherry']
 
-# 9. Appending to a List
+#  Appending to a List
 fruits.append("orange")
 print(fruits)  # Output: ['apple', 'banana', 'cherry', 'orange']
 
-# 10. Removing from a List
+#  Removing from a List
 fruits.remove("banana")
 print(fruits)  # Output: ['apple', 'cherry', 'orange']
 
-# 11. Sorting a List
+# Sorting a List
 numbers = [5, 2, 9, 1]
 numbers.sort()
 print(numbers)  # Output: [1, 2, 5, 9]
 
-# 12. Accessing List Elements by Index
+# Accessing List Elements by Index
 fruits = ["apple", "banana", "cherry"]
 print(fruits[1])  # Output: banana
